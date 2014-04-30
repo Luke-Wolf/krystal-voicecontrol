e320cda6443b4db587efd7beaa006b752c13e22b
Luke Wolf <lukewolf101010@gmail.com>
2014-04-30 01:10:30 -0700

    Refactor to use Krystal.Core

--- Krystal.Voice/Program.cs
+++ Krystal.Voice/Program.cs
@@ -4,17 +4,25 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Krystal.Core;
 
-namespace WindowsFormsApplication
+namespace Krystal.Voice
 {
 
 
     class MyPipeline : UtilMPipeline
     {
-        ProcessStartInfo startMusic = new ProcessStartInfo("..\\..\\Media\\Green Bird.mp3");
+
+        /*ProcessStartInfo startMusic = new ProcessStartInfo("..\\..\\Media\\Green Bird.mp3");
         ProcessStartInfo startMovie = new ProcessStartInfo("..\\..\\Media\\Frozen.mp4");
         ProcessStartInfo startPowerPoint = new ProcessStartInfo("..\\..\\Media\\Proposal.pptx");
-
+        */
+        readonly Controller controller = new Controller(new Core.ICommand[]
+        {
+            new PlayMusicCommand("..\\..\\Media\\Green Bird.mp3"),
+            new PlayVideoCommand("..\\..\\Media\\Frozen.mp4"),
+            new PlayPowerpointCommand("..\\..\\Media\\Proposal.pptx")
+        });
 
         public MyPipeline() : base()
         {
@@ -23,17 +31,17 @@ namespace WindowsFormsApplication
 
         public override void OnRecognized(ref PXCMVoiceRecognition.Recognition data)
         {
-            if (data.dictation.ToString().ToLower().Contains("music")) 
+            controller.Execute(data.dictation);
+            /*if (data.dictation.ToString().ToLower().Contains("music")) 
                 Process.Start(startMusic);
             if (data.dictation.ToString().ToLower().Contains("movie")) 
                 Process.Start(startMovie);
             if (data.dictation.ToString().ToLower().Contains("powerpoint"))
                 Process.Start(startPowerPoint);
-
+            */
             //Console.WriteLine("Recognized: " + data.dictation);
             //if (data.ToString() == "Play music") {}
         }
-
 
     }
 
